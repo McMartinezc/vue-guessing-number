9f3639b563043543a77b0ee02c9c3c08f1606cd5
McMartinezc <psyence_mc@hotmail.com>
2023-11-14 11:41:23 +0100

    versión vue

--- src/components/GuessNumber.vue
+++ src/components/GuessNumber.vue
@@ -1,56 +1,42 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 const randomNumber = ref(Math.floor(Math.random() * 100 + 1));
 const userValueInput = ref("");
 const previousGuesses = ref([]);
 const countAttempts = ref(10);
-const message = ref("");
-
 
 const checkNumber = () => {
-    const numberUser = +userValueInput.value;
-
-    if (countAttempts.value === 0) {
-        message.value = "No more attempts!";
-        return;
-    }
-
-    previousGuesses.value.push(numberUser);
-
-    if (numberUser > randomNumber.value) {
-        message.value = `Too High! Try again.`;
-    } else if (numberUser < randomNumber.value) {
-        message.value = `Too low! Try again.`;
-    } else {
-        message.value = `Correct number!`;
-    }
-
+    previousGuesses.value.push(userValueInput.value);
     countAttempts.value--;
-
-    if (countAttempts.value === 0) {
-        message.value = "No more attempts!";
-    }
-
     userValueInput.value = "";
 }
 
+const lastGuess = computed(() => {
+    return previousGuesses.value[previousGuesses.value.length - 1];
+})
 </script>
 
 <template>
-    <p>Try and guess a random number between 1 and 100.</p>
-    <p>You have {{countAttempts}} attempts to guess the right number.</p>
     <div id="wrapper">
-        <label for="guessField">Guess a number</label>
+        <p>Try and guess a random number between 1 and 100.</p>
+        <p>You have {{ countAttempts }} attempts to guess the right number.</p>
+
+        <label for="guessField">Guess a number:</label>
         <input type="number" id="guessField" v-model="userValueInput">
-        <label for="numberAttempts">Number of attempts</label>
+
+        <label for="numberAttempts">Number of attempts:</label>
         <input type="number" id="numberAttempts" v-model="countAttempts">
+
         <button class="button-check" @click="checkNumber">Check Guess</button>
 
-        <div class="resultParas">
+        <div v-if="previousGuesses.length" class="resultParas">
             <p>Previous Guesses: <span class="guesses">{{ previousGuesses }}</span></p>
             <p>Guesses Remaining: <span class="lastResult">{{ countAttempts }}</span></p>
-            <p class="lowOrHi">{{ message }}</p>
+            <p v-if="countAttempts === 0 && lastGuess !== randomNumber">YOU LOST 🤧</p>
+            <p style="color: red" v-else-if="lastGuess > randomNumber">Is lower</p>
+            <p style="color: green" v-else-if="lastGuess < randomNumber">Is higher</p>
+            <p v-else-if="lastGuess === randomNumber">You Won!! Correct number was {{ randomNumber }} </p>
         </div>
     </div>
 </template>
