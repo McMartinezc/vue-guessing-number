842b6bef5462b8ae45c8c076e9de003eed7739c6
McMartinezc <psyence_mc@hotmail.com>
2023-11-15 12:49:11 +0100

    falta hacer listening to events

--- src/components/GuessNumber.vue
+++ src/components/GuessNumber.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, computed } from "vue";
 
+
 const randomNumber = ref(Math.floor(Math.random() * 100 + 1));
 const userValueInput = ref("");
 const previousGuesses = ref([]);
@@ -15,6 +16,12 @@ const checkNumber = () => {
 const lastGuess = computed(() => {
     return previousGuesses.value[previousGuesses.value.length - 1];
 })
+
+//Deshabilita el input cuando se cumple la condición 
+const mustDisableInput = computed(()=>{
+    return countAttempts.value===0 || lastGuess.value===randomNumber.value;
+})
+
 </script>
 
 <template>
@@ -23,14 +30,14 @@ const lastGuess = computed(() => {
         <p>You have {{ countAttempts }} attempts to guess the right number.</p>
 
         <label for="guessField">Guess a number:</label>
-        <input type="number" id="guessField" v-model="userValueInput">
+        <input :disabled="mustDisableInput" type="number" id="guessField" v-model="userValueInput">
 
         <label for="numberAttempts">Number of attempts:</label>
         <input type="number" id="numberAttempts" v-model="countAttempts">
 
-        <button class="button-check" @click="checkNumber">Check Guess</button>
+        <button :disabled="!userValueInput" class="button-check" @click="checkNumber">Check Guess</button>
 
-        <div v-if="previousGuesses.length" class="resultParas">
+        <div v-show="previousGuesses.length != 0" class="resultParas">
             <p>Previous Guesses: <span class="guesses">{{ previousGuesses }}</span></p>
             <p>Guesses Remaining: <span class="lastResult">{{ countAttempts }}</span></p>
             <p v-if="countAttempts === 0 && lastGuess !== randomNumber">YOU LOST 🤧</p>
@@ -38,6 +45,9 @@ const lastGuess = computed(() => {
             <p style="color: green" v-else-if="lastGuess < randomNumber">Is higher</p>
             <p v-else-if="lastGuess === randomNumber">You Won!! Correct number was {{ randomNumber }} </p>
         </div>
+
+        <button class="button-reset" v-show="mustDisableInput" @click="startGame">New Game</button>
+
     </div>
 </template>
 
